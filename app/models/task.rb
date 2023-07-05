31bfd724951b4e7d2b964a32976a4a5127592ca9
elbub1979 <ilya.leuh09@gmail.com>
2023-07-06 00:19:06 +0300

    changing event names in state machine

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,32 +1,28 @@
 class Task < ApplicationRecord
 
   state_machine :state, initial: :new_task do
-    event :in_development do
-      transition new_task: :in_development
+    event :develop do
+      transition %i[new_task in_qa ready_for_release] => :in_development
     end
 
-    event :in_qa do
+    event :testing do
       transition in_development: :in_qa
     end
 
-    event :in_code_review do
+    event :review_the_code do
       transition in_qa: :in_code_review
     end
 
-    event :ready_for_release do
+    event :prepare_for_release do
       transition in_code_review: :ready_for_release
       end
 
-    event :released do
+    event :release do
       transition ready_for_release: :released
     end
 
-    event :archived do
+    event :archive do
       transaction %i[new_task released] => :archived
-    end
-
-    event :in_revision do
-      transaction %i[in_qa ready_for_release] => :in_development
     end
   end
 end

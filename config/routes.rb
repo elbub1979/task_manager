44e7099db76d42c50925980325699aa11810ca7f
elbub1979 <ilya.leuh09@gmail.com>
2023-07-27 23:54:20 +0300

    fix in routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,5 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :tasks, only: [:index, :show, :create, :update, :destroy]
     end
-
-  namespace :admin do
-    resources :users
   end
 end
